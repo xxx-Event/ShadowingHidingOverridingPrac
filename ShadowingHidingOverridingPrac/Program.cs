e2dfbe79a93c00723b70a55b9a8a46753c2bca94
xxx-Event <s2119@e-mirim.hs.kr>
2023-06-15 10:19:30 +0900

    초기 커밋.

---
+++ ShadowingHidingOverridingPrac/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowingHidingOverridingPrac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
